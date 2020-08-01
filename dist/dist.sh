08f7eebcd411d087e5c5eb0aea1b6259f88d6a7d
sam <sam@freighttrust.com>
2020-08-01 09:55:29 -0700

    build(gh-pages): gh-pages deploy

--- dist/dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z $NOSIGN ]; then
-  echo "==> Signing..."
-  shasum -a256 * > ./freight_${VERSION}_SHA256SUMS
-  gpg --default-key 6F6EB43E --detach-sig ./freight_${VERSION}_SHA256SUMS
-#  popd
-fi
